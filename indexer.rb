175e00b48b8b04fe0804409bdd49ac4c308be801
Piotr S. Staszewski <p.staszewski@gmail.com>
2012-06-22 16:29:26 +0200

    Added basic database support.

--- indexer.rb
+++ indexer.rb
@@ -8,7 +8,41 @@ VERSION = '0.0.1'
 
 %w{ digest find logger pp optparse rubygems filemagic dm-core }.each{|g| require g}
 
-options = { :loglevel => 1, :database => 'data.bin', :handlers => true, :root => false }
+###
+# Basic database model.
+# All main models are to be prefixed with 'St'.
+# 
+class Entry
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :digest, String, :required => true 
+  property :name, String, :required => true
+  property :path, String, :required => true
+  property :size, Integer, :required => true
+  property :mime, String
+  property :mtime, DateTime
+  property :ctime, DateTime
+
+  has n, :tags, :through => Resource
+end
+
+class Tag
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String, :required => true
+  property :ctime, DateTime, :default => Proc.new{|r,p| DateTime.now}
+
+  has n, :entries, :through => Resource
+end
+
+DataMapper.finalize
+
+###
+# Command-line script starts here.
+#
+options = { :loglevel => 1, :database => File.join(Dir.pwd, 'data.bin'), :handlers => true, :root => false }
 OptionParser.new do |o|
   o.banner = 'Usage: indexer.rb [options] tag1 dir1 tag2 dir2...'
 
@@ -44,29 +78,50 @@ log = Logger.new(STDOUT)
 log.formatter = proc{|s,d,p,m| "#{d.strftime('%H:%M:%S')} (#{s.ljust(5)}) #{m}\n"}
 log.level = options[:loglevel]
 
+DataMapper.setup(:default, 'sqlite://' + options[:database])
+unless File.exists? options[:database]
+  log.info 'Setting up fresh database...'
+  require 'dm-migrations'
+  DataMapper.auto_migrate!
+end
+
 fm = FileMagic.new(:mime)
 
 Hash[*ARGV].each do |tag,path|
   unless File.exists? path
-    log.error "No such path #{path}!"
-    log.error "Skipping #{tag} #{path}."
+    log.warn "No such path #{path}!"
+    log.warn "Skipping #{tag} #{path}."
     next
   end
   path += '/' unless path.end_with? '/'
   root = options[:root] || path
   unless path.start_with? root
-    log.error "Path #{path} is not located at root #{root}!"
-    log.error "Skipping #{tag} #{path}."
+    log.warn "Path #{path} is not located at root #{root}!"
+    log.warn "Skipping #{tag} #{path}."
     next
+  end
+  unless t = Tag.first(:name => tag)
+    t = Tag.new(:name => tag) 
+    t.save
   end
   log.info "Walking #{path}..."
   Find.find(path) do |p|
     next unless File.file? p
+    digest = Digest::MD5.file(p).hexdigest
     stat = File.stat(p)
-    mime = fm.file(p)
+    mime = fm.file(p).split(';').first
     path, name = File.split(p)
     path = '/' + (path.slice(root.length, path.length) || '')
-    puts "#{path.ljust(32)} #{name}"
-    puts "#{stat.size} #{stat.mtime} #{stat.ctime} #{mime}"
+    if e = Entry.first(:digest => digest)
+      log.warn "Duplicate file #{name} (digest #{digest})"
+      if e.name == name and e.path == path and e.tags.member?(t)
+        log.warn 'Name, path and tag match, skipping.'
+        next
+      end
+    end
+    e = Entry.new(:name => name, :path => path, :digest => digest, :mime => mime, \
+                  :size => stat.size, :mtime => stat.mtime, :ctime => stat.ctime)
+    e.save
+    EntryTag.new(:entry => e, :tag => t).save
   end
 end
